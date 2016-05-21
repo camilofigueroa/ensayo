164ddb4ba4dbfd08a01d04d1828c3b899d34bd66
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-05-21 03:06:30 -0500

    Add files via upload

---
+++ nodejs001.js
@@ -0,0 +1,24 @@
+
+/**
+ * Autor: Camilo Figueroa
+ * 20/05/2016
+ */
+
+/*Esta línea funcionó perfecto, hay que recordar que al compilar o al ejecutar se realiza con el comando básico
+Prompt command: node ruta_archivo/archivo
+*/
+/*console.log( "Aquí" );*/
+
+
+//Hora de crar un servidor
+var http = require( "http" );
+
+var manejador = function( solicitud, respuesta )
+{
+    console.log( "Hola mundo" );
+    respuesta.end( "Esto se deberá colocar en el navegador." );
+};
+
+var servidor = http.createServer( manejador );
+
+servidor.listen( 8080 );
