8b9db34d6fc7087a862fde6fb0da11f058d1ad6a
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-05-20 19:36:32 -0500

    Add files via upload

---
+++ nodejs.js
@@ -0,0 +1,10 @@
+
+
+
+/*Esta línea funcionó perfecto, hay que recordar que al compilar o al ejecutar se realiza con el comando básico
+Prompt command: node ruta_archivo/archivo
+*/
+console.log( "Aquí" );
+
+
+//Hora de crar un servidor
